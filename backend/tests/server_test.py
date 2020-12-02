c4619234de462b8b227a4958a6cfe8ff654da032
Hoeun Sim <h.sim312@gmail.com>
2020-12-01 21:27:58 -0800

    Testing (#27)
    * Add conftest and ingestion test
    
    * Add test for GraphQL query
    
    * Add tests for resolver functions

--- backend/tests/server_test.py
+++ backend/tests/server_test.py
@@ -3,8 +3,11 @@ from backend.extensions import db
 from backend.model import Incident, Location
 
 
-def test_run_query(app, client):
-    # Given: An incident exists in our DB
+def add_incident(app):
+    """
+    Helper function to load an incident into the DB
+    """
+
     with app.app_context():
         incident1 = Incident(external_id='1',
                              links=[],
@@ -16,8 +19,15 @@ def test_run_query(app, client):
                              city="Las Vegas",
                              state="Nevada",
                              location=Location(city='Las Vegas', state='Nevada'))
-        db.session.add(incident1)
-        db.session.commit()
+
+    db.session.add(incident1)
+    db.session.commit()
+
+
+def test_get_incidents(app, client):
+    # Given: An incident exists in our DB
+    with app.app_context():
+        add_incident(app)
 
         # When: We query for an incident
         query = """
@@ -37,6 +47,8 @@ def test_run_query(app, client):
                 """
 
         incidents_count = db.session.query(Incident).count()
+
+
     # Then: We should get back 1 result
     assert incidents_count == 1
 
@@ -55,3 +67,147 @@ def test_run_query(app, client):
                                                              'from a distance',
                                                      'state': 'Nevada',
                                                      'tags': '[]'}]}}
+
+
+def test_get_locations(app, client):
+    # Given: An incident (with a location) exists in our DB
+    with app.app_context():
+        add_incident(app)
+
+        # When: We query for locations
+        query = """
+                {
+                locations {
+                    id,
+                    city,
+                    state,
+                    lon,
+                    lat
+                    }
+                }
+                """
+
+        locations_count = db.session.query(Incident).count()
+
+    # Then: We should get back 1 result
+    assert locations_count == 1
+
+    rv = client.get('/graphql', json={'query': query})
+    # Then: The result should contain information about that location
+    assert rv.get_json() == {
+        "data": {
+            "locations": [
+                {
+                    "id": "1",
+                    "city": "Las Vegas",
+                    "state": "Nevada",
+                    "lon": None,
+                    "lat": None
+                }
+            ]
+        }
+    }
+
+
+def test_get_total_incidents(app, client):
+    # Given: Two incidents exists in our DB
+    with app.app_context():
+        add_incident(app)
+        add_incident(app)
+
+        # When: We query for total incidents
+        query = """
+                {
+                    getTotalIncidents
+                }
+                """
+
+        incidents_count = db.session.query(Incident).count()
+
+    # Then: We should get back 2 results
+    assert incidents_count == 2
+
+    rv = client.get('/graphql', json={'query': query})
+
+    # Then: The result should contain 2 as the value for 'getTotalIncidents'
+    assert rv.get_json() == {
+        "data": {
+            "getTotalIncidents": 2
+        }
+    }
+
+
+def test_get_distinct_states(app, client):
+    # Given: Two incidents from different states
+    with app.app_context():
+        add_incident(app)
+        incident2 = Incident(external_id='2',
+                             links=[],
+                             tags=[],
+                             date="2020-05-29",
+                             date_text="May 29th",
+                             name="Police arrest a man for speaking at them from a distance",
+                             description="Officer line moves protesters back and as protesters comply they attempt to arrest a man that was not visibly violent.",
+                             city="San Francisco",
+                             state="California",
+                             location=Location(city='San Francisco', state='California'))
+        db.session.add(incident2)
+        db.session.commit()
+
+        # When: We query for total distinct states
+        query = """
+                {
+                    getTotalDistinctStates
+                }
+                """
+
+        incidents_count = db.session.query(Incident).count()
+
+    # Then: We should get back 2 for our incidents count
+    assert incidents_count == 2
+
+    rv = client.get('/graphql', json={'query': query})
+
+    # Then: The result should indicate that we have a total of 2 distinct states
+    assert rv.get_json() == {
+        "data": {
+            "getTotalDistinctStates": 2
+        }}
+
+
+def test_get_distinct_cities(app, client):
+    # Given: Two incidents from different cities (but the same state) exist in our DB
+    with app.app_context():
+        add_incident(app)
+        incident2 = Incident(external_id='2',
+                             links=[],
+                             tags=[],
+                             date="2020-05-29",
+                             date_text="May 29th",
+                             name="Police arrest a man for speaking at them from a distance",
+                             description="Officer line moves protesters back and as protesters comply they attempt to arrest a man that was not visibly violent.",
+                             city="Reno",
+                             state="Nevada",
+                             location=Location(city='Reno', state='Nevada'))
+        db.session.add(incident2)
+        db.session.commit()
+
+        # When: We query for total distinct cities
+        query = """
+                {
+                    getTotalDistinctCities
+                }
+                """
+
+        incidents_count = db.session.query(Incident).count()
+
+    # Then: We should get back 2 for our incidents count
+    assert incidents_count == 2
+
+    rv = client.get('/graphql', json={'query': query})
+
+    # Then: The result should indicate that we have a total of 2 distinct cities
+    assert rv.get_json() == {
+        "data": {
+            "getTotalDistinctCities": 2
+        }}
